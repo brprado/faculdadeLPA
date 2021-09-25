a6bcc093d0176d55e391dcef5b8e3887d887f5b1
Bruno Carvalho do Prado <brunopradocode@gmail.com>
2021-09-25 11:51:54 -0300

    Update pratica014.java

--- pratica014.java
+++ pratica014.java
@@ -10,7 +10,6 @@ public class Pratica014 {
 		System.out.println("MAIOR E MENOR VALOR");
 		System.out.println("Os valores devem estar entre 10 e 90");
 		
-		
 		System.out.println("Informe o primeiro valor");
 		n1 = input.nextDouble();
 		maior = n1;
@@ -26,7 +25,6 @@ public class Pratica014 {
 			n2 = menor;
 		}
 		
-		
 		System.out.println("Informe o terceiro valor");
 		n3 = input.nextDouble();
 		
@@ -39,32 +37,6 @@ public class Pratica014 {
 		
 		if (n1 < 10 || n1 > 90 || n2 < 10 || n2 > 90 || n3 < 10 || n3 > 90){
 			System.out.println("VALORES INCORRETOS");
-			if (n1 < 10 || n1 > 90) {
-				System.out.println("O numero " + n1 + " não está no intervalo entre 10 e 90");
-			
-			}else if (n2 < 10 || n2 > 90) {
-				System.out.println("O numero " + n2 + " não está no intervalo entre 10 e 90");
-			
-			}else{
-				System.out.println("O numero " + n3 + "não está no intervalo entre 10 e 90");
-			}
-			
-		}else{
-			System.out.println("Voce escolheu os numeros " + n1 + " " + n2 + " " + n3 );
-			System.out.println("O maior número é " + maior);
-			System.out.println("O menor número é " + menor);
-		}
-		input.close();
-	}
-
-}
-		
-		}else if(n3 < menor){
-			menor = n3;
-		}
-		
-		if (n1 < 10 || n1 > 90 || n2 < 10 || n2 > 90 || n3 < 10 || n3 > 90){
-			
 			if (n1 < 10 || n1 > 90) {
 				System.out.println("O numero " + n1 + " não está no intervalo entre 10 e 90");
 			
