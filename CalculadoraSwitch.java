af981a8f00eaa4869bed77a668f2e8b14f74e122
Bruno Carvalho do Prado <brunopradocode@gmail.com>
2021-10-05 08:28:52 -0300

    Update CalculadoraSwitch.java

--- CalculadoraSwitch.java
+++ CalculadoraSwitch.java
@@ -6,19 +6,19 @@ public class CalculadoraSwitch {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.print("Informe um numero: ");
-		int n1 = (int) input.nextDouble();
-		
+		double n1 =  input.nextDouble();
+
 		System.out.print("Informe um numero: ");
-		int n2 = (int) input.nextDouble();
-		
+		double n2 =  input.nextDouble();
+
 		System.out.println("Informe a operação que deseja realizar");
 		System.out.println("SOMA[1] SUBTRAÇÃO[2] DIVISÃO[3] MULTIPLICAÇÃO[4]: ");
 		int operacao = input.nextInt();
-		
+
 		if(n1 > 0 && n2 > 0) {
-			
+
 			switch (operacao) {
 			case 1:
 				System.out.println("soma = " + (n1 + n2));
@@ -27,9 +27,7 @@ public class CalculadoraSwitch {
 				System.out.println("subtração = " + (n1 - n2));
 				break;
 			case 3:
-				float n1Div = n1;
-				float n2Div = n2;
-				System.out.println("divisão = " + (n1Div / n2Div));
+				System.out.println("divisão = " + (n1 / n2));
 				break;
 			case 4:
 				System.out.println("multiplicação = " + (n1 * n2));
@@ -38,8 +36,8 @@ public class CalculadoraSwitch {
 				System.out.println("OPERAÇÃO INVÁLIDA");
 				break;
 			}
-			
-		
+
+
 		}else {
 			System.out.println("Informes valores maiores do que 1");
 		}
