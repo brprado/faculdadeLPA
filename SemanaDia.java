46a3414970111f53253649c4af78309c269e91ad
Bruno Carvalho do Prado <brunopradocode@gmail.com>
2021-10-05 07:32:18 -0300

    Create SemanaDia.java

---
+++ SemanaDia.java
@@ -0,0 +1,44 @@
+package br.ucsal;
+
+import java.util.Scanner;
+
+public class Semana {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println();
+		
+		System.out.println("Informe o número do dia da semana \ne veja o nome correspondente");
+		int diaSemana = input.nextInt();
+			
+		switch (diaSemana) {
+			case 1:
+				System.out.println("DOMINGO");
+				break;
+			case 2:
+				System.out.println("SEGUNDA");
+				break;
+			case 3:
+				System.out.println("TERÇA");
+				break;
+			case 4:
+				System.out.println("QUARTA");
+				break;
+			case 5:
+				System.out.println("QUINTA");
+				break;
+			case 6:
+				System.out.println("SEXTA");
+				break;
+			case 7:
+				System.out.println("SÁBADO");
+				break;
+			default:
+				System.out.println("Dia inválido. Tente novamente");
+				break;
+				
+			}
+		input.close();
+		}
+		
+		}
