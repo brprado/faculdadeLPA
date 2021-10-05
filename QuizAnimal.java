8eeb54289e04610698bc742e5d54302be7d80085
Bruno Prado <brunopradocode@gmail.com>
2021-10-05 09:03:31 -0300

    Create QuizAnimal.java

---
+++ QuizAnimal.java
@@ -0,0 +1,38 @@
+package br.ucsal;
+
+import java.util.Scanner;
+
+public class QuestionarioAnimal {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Indique entre as opções um animal invertebrado");
+		System.out.println("QUESTÃO 01:\n(a)MINHOCA\n(b)ARANHA\n(c)GATO\n(d)CACHORRO\n\nSUA OPÇÃO: ");
+		String animalEscolhido = input.nextLine().trim().toLowerCase();
+		
+		switch (animalEscolhido) {
+		case "a": {
+			System.out.println("VOCÊ ACERTOU");
+			break;
+		}
+		case "b": {
+			System.out.println("VOCÊ ACERTOU");
+			break;
+		}
+		case "c": {
+			System.out.println("VOCÊ ERROU");
+			break;
+		}
+		case "d": {
+			System.out.println("VOCÊ ERROU");
+			break;
+		}
+		default:
+			System.out.println("Opção inválida!");
+			break;
+		}
+		input.close();
+		
+	}
+
+}
