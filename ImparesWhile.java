6ba54c420a90531ddcbea059cc91094d53a96b36
Bruno Prado <brunopradocode@gmail.com>
2021-10-08 07:54:01 -0300

    Create ImparesWhile.java

---
+++ ImparesWhile.java
@@ -0,0 +1,18 @@
+package br.ucsal;
+
+public class While {
+
+	public static void main(String[] args) { //while(condição) {código}
+		int var = 1;
+		while (var < 50){
+			var++;
+			if(var % 2 == 1) {
+				System.out.println(var);
+			}
+			
+		}
+		
+
+	}
+
+}
