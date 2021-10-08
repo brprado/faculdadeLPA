c0217647e8925ba3ebd4412e8c2fc2646d6e4445
Bruno Prado <brunopradocode@gmail.com>
2021-10-08 07:34:30 -0300

    Create WhilePrática18.java

---
+++ WhilePrática18.java
@@ -0,0 +1,15 @@
+package br.ucsal;
+
+public class While {
+
+	public static void main(String[] args) { //while(condição) {código}
+		int var = 1;
+		while (var <= 50){
+			System.out.println(var);
+			var++;
+		}
+		
+
+	}
+
+}
