54f5e097edcdb1bc7fcb42fe78e093159b2a769f
Bruno Prado <brunopradocode@gmail.com>
2021-10-08 08:11:54 -0300

    Create MultiplosDois.java

---
+++ MultiplosDois.java
@@ -0,0 +1,14 @@
+package br.ucsal;
+
+public class While {
+
+	public static void main(String[] args) { //while(condição) {código}
+		int var = 1;
+		int cont = 0;
+		
+		while (cont < 12){
+			System.out.println(2 * var);
+			cont++;
+			var++;
+		}
+	}
