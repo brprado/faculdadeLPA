a922a899ddd4183a6f84d8345e9833904cb6a060
Bruno Prado <brunopradocode@gmail.com>
2021-10-08 08:05:12 -0300

    Update ImparesWhile.java

--- ImparesWhile.java
+++ ImparesWhile.java
@@ -4,7 +4,7 @@ public class While {
 
 	public static void main(String[] args) { //while(condição) {código}
 		int var = 1;
-		while (var < 50){
+		while (var <= 50){
 			var++;
 			if(var % 2 == 1) {
 				System.out.print(var);
