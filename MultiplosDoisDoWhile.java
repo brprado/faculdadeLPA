5efe16d82bbd0c3a6b170834bcb8982c7a4371e1
Bruno Prado <brunopradocode@gmail.com>
2021-10-19 07:38:52 -0300

    Update MultiplosDoisDoWhile.java

--- MultiplosDoisDoWhile.java
+++ MultiplosDoisDoWhile.java
@@ -7,12 +7,12 @@ public class Repeticao {
 		int valor = 2;
 		int cont = 1;
 		System.out.println("Os multiplos de dois são: ");
+		
 		do {
-
+	
 			System.out.println( valor * cont);
 			cont++;
-			
-
+		
 		}while(cont < 13);
 
 	}
