3a33b55bd788cc9bd5d0deb6a2162625042403b2
Bruno Prado <brunopradocode@gmail.com>
2021-10-19 07:46:38 -0300

    Create DivisiveisTresDoWhile.java

---
+++ DivisiveisTresDoWhile.java
@@ -0,0 +1,24 @@
+package br.ucsal;
+
+public class Repeticao {
+
+	public static void main(String[] args) {
+
+		int valor = 99;
+		int cont = 0;
+		System.out.println("Menores de 99 divisiveis por 3: ");
+		
+		do {
+			
+      if(valor % 3 == 0) {
+				cont++;
+				System.out.println(valor);
+			}
+      
+			valor--;
+			
+
+		}while(cont < 15);
+
+	}
+}
