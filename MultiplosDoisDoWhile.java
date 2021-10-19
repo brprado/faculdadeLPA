61c3dcec9a597923ab669106410e00974b4a98d1
Bruno Prado <brunopradocode@gmail.com>
2021-10-19 07:27:26 -0300

    Create MultiplosDoisDoWhile.java

---
+++ MultiplosDoisDoWhile.java
@@ -0,0 +1,19 @@
+package br.ucsal;
+
+public class Repeticao {
+
+	public static void main(String[] args) {
+
+		int valor = 2;
+		int cont = 1;
+		do {
+
+			System.out.println(valor + " x " + cont 
+					+ " = " + valor * cont);
+			cont++;
+			
+
+		}while(cont < 13);
+
+	}
+}
