983fc6a2b2c8da146703fbd1dc32380ad17bd2e4
Bruno Prado <brunopradocode@gmail.com>
2021-11-24 09:40:51 -0300

    Update Pratica32.java

--- Pratica32.java
+++ Pratica32.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class Pratica32 {
 
-    public static void printarArray(){
+    public static String printarArray(){
         int[] minhaArray = new int[30];
         for (int i = 0, num = 1; i < minhaArray.length; i++, num++){
             minhaArray[i] = num;
         }
-        System.out.println(Arrays.toString(minhaArray));
+        return Arrays.toString(minhaArray);
     }
 
     public static void main(String[] args) {
-        printarArray();
+        System.out.println(printarArray());
 
     }
 }
