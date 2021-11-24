dadbd8655bec366b8fe47dec3cf480902774c85a
Bruno Prado <brunopradocode@gmail.com>
2021-11-24 09:21:44 -0300

    24/11/2021

---
+++ ArrayNumerosPares.java
@@ -0,0 +1,15 @@
+package faculdade;
+
+public class ArrayNumerosPares {
+
+    public static void main(String[] args) {
+
+        int[] minhaArray = new int[15];
+        for (int i = 0, num = 2; i < 15; i++, num += 2){
+            System.out.print(num + " ");
+        }
+
+
+
+    }
+}
